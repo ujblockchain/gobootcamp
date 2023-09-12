ea143cc8563cc10af147cfd4c459767f6688e26c
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-12 15:20:21 +0200

    boolean

---
+++ 05-boolean/boolean.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a variable is a placeholder, a container to hold something
+	// "var" is used to declare variable
+
+	// different ways to declare a boolean variables
+	var isStudent = true
+	// var isStudent bool = true
+	// var isStudent := true
+
+	//printing boolean
+	fmt.Println(isStudent)
+
+	//
+	isFinalYear := false
+	fmt.Println(isFinalYear)
+
+}
