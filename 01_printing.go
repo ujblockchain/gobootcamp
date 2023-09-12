d3cd4bef3fa51bb1ff335dc9aae9a94d4b64505d
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-12 11:02:48 +0200

    printing text

---
+++ 01_printing.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// using fmt Print,
+	// Print does not add a new line
+	fmt.Print("Hello World")
+	fmt.Print("Welcome")
+
+	// using print with "\n"
+	// this puts each in a new line
+	fmt.Print("Hello World \n")
+	fmt.Print("Welcome \n")
+
+	//using fmt Println
+	// this puts it in a new line
+	fmt.Println("Hello World")
+
+	// print your name
+	fmt.Println("My name is John Deo")
+
+	//printing your department
+	fmt.Println("My department is Electrical & Electronic Engineering")
+}
