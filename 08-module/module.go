fb60c84a1df0beda6b43b3d40d3ad067e9309862
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-13 12:09:49 +0200

    module

---
+++ 08-module/module.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Create a github account and a repository")
+	fmt.Println("run `go mod init github.com/username/repo name` in the terminal")
+	fmt.Println("replace username and repo name with yours")
+
+}
