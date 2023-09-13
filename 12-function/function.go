4b29b3150c51f4b1183e9a453fd9415917ab8dd4
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-13 16:09:26 +0200

    add return in function

--- 12-function/function.go
+++ 12-function/function.go
@@ -15,6 +15,16 @@ func main() {
 
 	//call function with two arguments
 	myBio(name, university)
+
+	//function with return 
+	sum := addTwoNumbers(78, 89)
+	fmt.Println(sum)
+	
+	//two return in function
+	myName, age := nameAge("James Mel", 45)
+	fmt.Printf("My name is : %v \n", myName)
+	fmt.Printf("My age is : %v \n", age)
+
 }
 
 // function with a parameter
@@ -26,3 +36,16 @@ func checkAttendance(personName string) {
 func myBio(name string, university string) {
 	fmt.Printf("My name is %v and I am studying in %v \n", name, university)
 }
+
+//using return in function
+func addTwoNumbers(numberOne int64, numberTwo int64) int64{
+	sum := numberOne + numberTwo
+	return sum
+}
+
+//returning two or more
+//function simply returns name and age
+func nameAge(name string, age int32) (string, int32){
+	return name, age
+}
+
