34343c26671b6862bf188076daf26c1add179463
edmundrotimi <ujsarchiblockchain@gmail.com>
2023-09-13 11:37:33 +0200

    using rune and byte

---
+++ 06-rune_byte/byte.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a variable is a placeholder, a container to hold something
+	// "var" is used to declare variable
+
+	// declaring a byte variable
+	var vowel byte = 'a'
+
+	//printing byte storage number
+	fmt.Println(vowel)
+
+	//printing byte value
+	fmt.Println(string(vowel))
+
+	//more example
+	var favoriteLetter byte = 'I'
+
+	//printing byte storage number
+	fmt.Println(favoriteLetter)
+
+	//printing byte value
+	fmt.Println(string(favoriteLetter))
+
+}
---
+++ 06-rune_byte/rune.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a variable is a placeholder, a container to hold something
+	// "var" is used to declare variable
+
+	// different ways to declare a rune variable
+	var countryCurrency = '€'
+	// var countryCurrency rune = '€'
+	// var countryCurrency := '€'
+
+	//printing rune storage number
+	fmt.Println(countryCurrency)
+
+	//printing rune value
+	fmt.Println(string(countryCurrency))
+
+	//more example
+	favoriteEmoji := '😇'
+
+	//printing rune storage number
+	fmt.Println(favoriteEmoji)
+
+	//printing rune value
+	fmt.Println(string(favoriteEmoji))
+
+}
